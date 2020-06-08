e990b5668feea9e7bb98adbfe3a589032c0d3931
frangortari <65463001+frangortari@users.noreply.github.com>
2020-06-08 20:38:16 -0300

    Update envia.php

--- envia.php
+++ envia.php
@@ -1,16 +1,16 @@
-﻿<?php
+<?php
 $remitente = $_POST['email'];
-$destinatario = 'taller9poetas@gmail.com'; // en esta línea va el mail del destinatario.
-$asunto = 'Dejanos tus comentarios'; // acá se puede modificar el asunto del mail
+$destinatario = 'mail@dominio.com'; // en esta línea va el mail del destinatario.
+$asunto = 'Consulta'; // acá se puede modificar el asunto del mail
 if (!$_POST){
 ?>
 
 <?php
 }else{
 
-    $cuerpo = "Nombre: " . $_POST["nombre"] . "\r\n";
+    $cuerpo = "Nombre y apellido: " . $_POST["nombre"] . "\r\n";
     $cuerpo .= "Email: " . $_POST["email"] . "\r\n";
-	$cuerpo .= "Dejanos tus comentarios: " . $_POST["consulta"] . "\r\n";
+	$cuerpo .= "Consulta: " . $_POST["consulta"] . "\r\n";
 	//las líneas de arriba definen el contenido del mail. Las palabras que están dentro de $_POST[""] deben coincidir con el "name" de cada campo.
 	// Si se agrega un campo al formulario, hay que agregarlo acá.
 
@@ -26,3 +26,4 @@ if (!$_POST){
     include 'confirma.html'; //se debe crear un html que confirma el envío
 }
 ?>
+
